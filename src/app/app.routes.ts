e05656a1b67bdc7b59ea53b2f1846d509a11b7cd
zotel1 <zotelsigel@gmail.com>
2025-11-14 18:59:29 -0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Chatbot } from './components/chatbot/chatbot';  // ajustá la ruta si tu carpeta se llama distinto
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Chatbot,        // ruta raíz -> muestra el chatbot
+  },
+  {
+    path: '**',
+    redirectTo: '',            // cualquier ruta rara redirige al chatbot
+  },
+];
